5abc4a4d5ee7b7f2f90b6fb2be42792cde829f6a
Leonid Chirkov <leonidchirkovul@gmail.com>
2023-03-14 01:01:25 +0600

    4: dashboard_controller

---
+++ spec/requests/dashboard_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Dashboard", type: :request do
+  describe "GET /dashboard" do
+    context "when not sign in" do
+      it 'is respond with redirect' do
+        get dashboard_path
+        expect(response).to have_http_status(:redirect)
+      end 
+    end
+    context "when logged in" do
+      it 'is succesful' do
+        user = create(:user)
+        sign_in user
+        get dashboard_path
+        expect(response).to have_http_status(:success)
+      end 
+    end
+  end
+end
