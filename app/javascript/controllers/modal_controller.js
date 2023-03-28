0a02e0118b95139bee04e93e97765130a3d3556c
Leonid Chirkov <leonidchirkovul@gmail.com>
2023-03-28 21:17:42 +0600

    13

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect() {
-    //console.log("Hello, Stimulus!")
     this.element.addEventListener('turbo:submit-end', () => {
       document.getElementById('close-model-btn').click();
       this.element.reset();
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe User, type: :model do
   it { is_expected.to have_many(:tweets).dependent(:destroy) }
 
   it { is_expected.to validate_uniqueness_of(:username).case_insensitive.allow_blank }
+
+  describe "#set_display_name" do
+    context "when display name is set" do
+      it "does not change the display_name" do
+        user = build(:user, username: "john", display_name: "John")
+        user.save
+        expect(user.reload.display_name).to eq("John")
+      end
+    end
+
+    context "when display name is not set" do
+      it "humanizes the previosly set username" do
+        user = build(:user, username: "john", display_name: nil)
+        user.save
+        expect(user.reload.display_name).to eq("John")
+      end
+    end
+  end
 end
