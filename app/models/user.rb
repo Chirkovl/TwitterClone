1f0eafc83bd866a80685cab5db95e3fba3921066
Leonid Chirkov <leonidchirkovul@gmail.com>
2023-03-28 22:11:19 +0600

    14: add user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets, dependent: :destroy
+  has_one_attached :avatar
 
   validates :username, uniqueness: { case_sensitive: false }, allow_blank: true
 
