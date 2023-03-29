8a9ee4150915a6ace830242aa36dd67a0e51f95e
Leonid Chirkov <leonidchirkovul@gmail.com>
2023-03-29 19:56:09 +0600

    Add tweet_presenter and rewrite logic for it

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   
-  def index 
+  def index
+    @tweets = Tweet.order(created_at: :desc).map { |tweet| TweetPresenter.new(tweet)}
   end
 end
